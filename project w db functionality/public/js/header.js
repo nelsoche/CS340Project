39ec1429850c6fa65a7005099aae4ad9f7b083cf
Chelsea <59673273+nelsoche@users.noreply.github.com>
2020-07-26 14:09:32 -0700

    Add files via upload

---
+++ project w db functionality/public/js/header.js
@@ -0,0 +1,10 @@
+var navContainer = document.getElementById("navMenu");
+var navItems = btnContainer.getElementsByClassName("btn");
+
+for (var i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
