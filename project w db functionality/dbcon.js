a5e1bbbccb11e503d62dea39c1f24e4f67c774d8
Chelsea <59673273+nelsoche@users.noreply.github.com>
2020-07-26 14:06:23 -0700

    Update dbcon.js

--- project w db functionality/dbcon.js
+++ project w db functionality/dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
 	connectionLimit : 10,
 	host            : 'classmysql.engr.oregonstate.edu',
-	user            : 'cs340_nelsoche',
-	password        : 'Mbmrkd317.',
-    database        : 'cs340_nelsoche'
+	user            : 'cs340_username',
+	password        : 'password',
+    database        : 'cs340_username'
 });
 module.exports.pool = pool;
