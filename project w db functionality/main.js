b26c422eb752bb0a0b32910a1f0d951dae002e9f
Chelsea <59673273+nelsoche@users.noreply.github.com>
2020-08-01 15:08:27 -0700

    Add files via upload

---
+++ project w db functionality/main.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var mysql = require('./dbcon.js');
+
+handlebars.handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+	return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+app.set('mysql', mysql);
+app.use('/characters', require('./characters.js'));
+app.use('/char_update', require('./charUpdate.js'));
+app.use('/locations', require('./locations.js'));
+/*app.use('/races', require('./races.js'));
+app.use('/weapons', require('./weapons.js'));
+app.use('/media', require('./media.js'));*/
+
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('plain/text');
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
